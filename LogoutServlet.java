8c9f1b5cffecf12c0e443b00e85c3847efbbc7d3
sohini29 <sohini.pupun@gmail.com>
2025-12-24 12:55:21 +0530

    LogoutServlet.java

---
+++ LogoutServlet.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        HttpSession session = req.getSession(false);
+
+        if (session != null) {
+            session.invalidate();   // destroy session
+        }
+
+        // Redirect after logout
+        res.sendRedirect("index.html"); 
+    }
+}
