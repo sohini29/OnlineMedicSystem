bbd37b38896f82287b0b923e25e71693f477cd84
sohini29 <sohini.pupun@gmail.com>
2025-12-24 12:56:44 +0530

    LogoutServlet3.java

---
+++ LogoutServlet3.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/LogoutServlet3")
+public class LogoutServlet3 extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        HttpSession session = req.getSession(false); // Get existing session
+        if(session != null) {
+            session.invalidate(); // Destroy session
+        }
+
+        // Redirect user to the home/login page
+        res.sendRedirect("index.html"); // Or patient_login.html / doctor_login.html depending on entry point
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        doGet(req, res); // Handle POST same as GET
+    }
+}
